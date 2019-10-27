8918b03f49b5e9333dec5802488c2354f0faef1a
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-10-26 20:28:46 -0500

    Se sube código con errores para revisar

--- keacGame/Components/HangedGame/HangedGame.js
+++ keacGame/Components/HangedGame/HangedGame.js
@@ -7,100 +7,98 @@ import BlankSpace from '../BlankSpace/BlankSpace'
 export default class Hanged extends Component {
     constructor(props){
         super(props);
-          
+
           let word = this.getGuessWord();
 
         this.state = {
             failures: 0,
-            // rightChoices: 0,
-             guessWord: word,
-            // //Desde aquí se pasa el argumento a la función para ejecutarla "word";
-             wordGuessSoFar: this.initializaeWordGuessSoFar(word),
-            // keyboard: this.getKeyboard(),
-            // modalVisible: false,
-            // result:''
+            rightChoices: 0,
+            guessWord: word,
+            //Desde aquí se pasa el argumento a la función para ejecutarla "word";
+            wordGuessSoFar: this.initializaeWordGuessSoFar(word),
+            keyboard: this.getKeyboard(),
+            modalVisible: false,
+            result: ""
         }
     }
 
-  // restart= ()=>{
-  //   let word = this.getGuessWord();
+  restart= ()=>{
+    let word = this.getGuessWord();
 
-  //   this.setState= {
-  //     rightChoices: 0,
-  //     failures: 0,
-  //     guessWord: word,
-  //     wordGuessSoFar: this.initializaeWordGuessSoFar(word),
-  //     keyboard: this.getKeyboard(),
-  //     modalVisible: false,
-  //     result: ' '
-  //   }
-  // }
+    this.setState= ({
+      rightChoices: 0,
+      failures: 0,
+      guessWord: word,
+      wordGuessSoFar: this.initializaeWordGuessSoFar(word),
+      keyboard: this.getKeyboard(),
+      modalVisible: false
+    });
+  }
   
-  // getKeyboard=()=>{
-  //   let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-  //                  'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q',
-  //                  'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  getKeyboard=()=>{
+    let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                   'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q',
+                   'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-  //   return letters.map((l)=>({letter: l, state: "not pressed"}));
-  // }
+    return letters.map((l)=>({letter: l, state: "not pressed"}));
+  }
 
-  // pressedButton = (i)=> {
-  //   let keyboard= this.state.keyboard;
-  //   let failures = this.state.failures;
-  //   let letter = this.state.keyboard[i].letter;
+  pressedButton = (i)=> {
+    let keyboard= this.state.keyboard;
+    let letter = this.state.keyboard[i].letter;
+    let failures = this.state.failures;
 
-  //   if(this.correctChoice(letter)){
-  //     keyboard[i].state = "rigth pressed";
-  //     this.setState({
-  //       keyboard: keyboard
-  //     });
-  //   } else {
-  //     keyboard[i].state = "wrong pressed";
-  //     this.setState({
-  //       keyboard: keyboard,
-  //       failures: ++failures
-  //     });
-  //   }   
-  // }
+    if(this.correctChoice(letter)){
+      this.state.keyboard[i].state = "rigth pressed";
+      this.setState({
+        keyboard: keyboard
+      });
+    } else {
+      this.state.keyboard[i].state = "wrong pressed";
+      this.setState({
+        keyboard: keyboard,
+        failures: ++failures
+      });
+    }   
+  }
 
-  // correctChoice = (letter) => {
-  //   let rigthChoice = false;
+  correctChoice = (letter) => {
+    let rigthChoice = false;
+    for(let i = 0; i > this.state.guessWord.length; i++){
+      if(letter == this.state.guessWord.substr(i, 1)){
+        this.setState({
+        rightChoices: ++rightChoices,
+        wordGuessSoFar: wordGuessSoFar.substr(0, i) +
+                        letter +
+                        wordGuessSoFar.substr(i + 1)
 
-  //   for(let i = 0; i > this.state.guessWord.length; i++){
-  //     if(letter == this.state.guessWord.substr(i, 1)){
-  //       this.setState({
-  //       rightChoices: ++rightChoices,
-  //       wordGuessSoFar: [... wordGuessSoFar.substr(0, i) +
-  //                       letter] +
-  //                       [...wordGuessSoFar.substr(i + 1)]
+        });
+        rigthChoice = true;
+      }
+    }
+    return rigthChoice;
+  }
 
-  //       });
-  //       rigthChoice = true;
-  //     }
-  //   }
-  //   return rigthChoice;
-  // }
+  componentDidUpdate(){
+    if(this.state.failures == 6){
+      this.setState({
+        modalVisible: true,
+        result: 0,
+        failures:0
+      })
+    }
 
-  // componentDidUpdate(){
-  //   if(this.state.failures == 5){
-  //     this.setState({
-  //       modalVisible: true,
-  //       result: '¡Perdiste!',
-  //       failures: 0
-  //     })
-  //   }
-
-  //   if(this.state.guessWord.length == this.state.rightChoices){
-  //     this.setState({
-  //       modalVisible: true,
-  //       result: '¡Ganaste!',
-  //       rightChoices:0
-  //     })
-  //   }
-  // }
+    if(this.state.guessWord.length == this.state.rightChoices){
+      this.setState({
+        modalVisible: true,
+        result: '¡Ganaste!',
+        rightChoices: 0
+      })
+    }
+  }
 
   getGuessWord= () => {
-      let words = ["ESDRUJULA","VELERO", "ACERTIJO", "SOMBRERO"];
+      let words = ["ESDRUJULA", "ACERTIJO", "SOMBRERO"];
       return words[Math.floor(Math.random() * words.length)];
 
   }
@@ -112,35 +110,35 @@ export default class Hanged extends Component {
       }
       return initializaeWord;
   }
+  //////
 
-  // getModal=()=>{
-  //   return(
-  //     <Modal animationType= "fade"
-  //            transparent = {true}
-  //            visible={this.state.modalVisible}
-  //            onRequestClose={()=> {this.reiniciar()}}>
-  //       <View style={styles.modalContainer}>
-  //         <View style={styles.innerModalContainer}>
-  //           <Text style = {styles.texto}>{this.state.outcome}
-  //           </Text>
-  //           <TouchableHighlight/>
-  //         </View>
-  //       </View>
-  //     </Modal>
-    
-  //   )
-  // }
+  getModal=()=>{
+    return(
+      <Modal animationType= "fade"
+             transparent = {true}
+             visible={this.state.modalVisible}
+             onRequestClose={()=> this.reiniciar()}>
+        <View style={styles.modalContainer}>
+          <View style={styles.innerModalContainer}>
+            <Text style = {styles.texto}>{this.state.result}
+            </Text>
+            <TouchableHighlight onPress={() => this.restart()}
+                                style = {styles.button}>
+              <Text style = {styles.textButton}>Pulsa para jugar de nuevo</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+    )
+  }
 
   
 
   render(){ 
-    console.log(this.state.failures)
-    console.log(this.state.guessWord)
-    console.log(this.state.wordGuessSoFar)
   return (
     
     <View style={styles.container}>
-      {/* {this.getModal()} */}
+      {this.getModal()}
       <View style={styles.imageContainer}>
           <Keac failures= {this.state.failures} />
       </View>
@@ -148,14 +146,12 @@ export default class Hanged extends Component {
          <BlankSpace wordGuessSoFar = {this.state.wordGuessSoFar} /> 
       </View>
       <View style={styles.keyboard}>
-          {/* <Keyboard keyboard = {this.state.keyboard} */}
-                    {/* pressedButton = {this.pressedButton}/> */}
-      </View>   
+          <Keyboard keyboard = {this.state.keyboard}
+                    pressedButton = {() => this.pressedButton(i)}/>
+      </View>    
     </View>
   );
   }
-
-  
 }
 
  const styles = StyleSheet.create({
@@ -169,16 +165,53 @@ export default class Hanged extends Component {
   imageContainer:{
     backgroundColor: 'green',
     flex: 5,
+    justifyContent: 'center'
   },
 
   blankSpace:{
     backgroundColor: 'red',
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
   keyboard:{
     backgroundColor: 'orange',
     flex: 3,
-  } 
-});
+    justifyContent: 'center'
+  },
+  
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)'
+  },
 
+  innerModalContainer: {
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor:'white',
+    margin: 20
+  },
+
+  button: {
+    flexWrap: 'wrap',
+    padding:10,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    margin: 20
+  },
+ 
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 20
+  },
+
+  textButton: {
+    fontSize: 20,
+    color: 'white'
+  }
+});
